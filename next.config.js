299624ce3e17fb04732f05c53424f7031c1c90cd
andreybyrsa <byrsa2004@bk.ru>
2023-12-02 15:23:26 +0500

    Настроил конфиг проекта

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const path = require('path')
+
+const nextConfig = {
+  sassOptions: {
+    additionalData: `@import "./src/styles/main.scss";`,
+  },
+}
 
 module.exports = nextConfig
