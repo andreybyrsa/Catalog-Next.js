f52104be5a967da1ffb5b4791bf6da0a0e0cd1b0
andreybyrsa <byrsa2004@bk.ru>
2023-12-03 21:15:06 +0500

    Добавил новые подстановочные данные

--- public/index.ts
+++ public/index.ts
@@ -1,5 +1,19 @@
 import mac from './mac.webp'
-import scaner from './scaner.webp'
+import canonScaner from './canonScaner.webp'
 import asustor from './asustor.webp'
+import mieleCoffee from './mieleCoffee.webp'
+import boschCoffee from './boschCoffee.webp'
+import DJI from './DJI.webp'
+import HPScaner from './HPScaner.webp'
+import nivonaCoffee from './nivonaCoffee.webp'
 
-export { mac, asustor, scaner }
+export {
+  mac,
+  asustor,
+  canonScaner,
+  boschCoffee,
+  DJI,
+  HPScaner,
+  nivonaCoffee,
+  mieleCoffee,
+}
