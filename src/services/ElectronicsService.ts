ffb69cdd5ed634bbfb19264f754a1141473f8fc7
andreybyrsa <byrsa2004@bk.ru>
2023-12-03 21:18:12 +0500

    Добавил сервис получения товаров по query страницы

--- src/services/ElectronicsService.ts
+++ src/services/ElectronicsService.ts
@@ -5,6 +5,20 @@ import { mocks } from '@Utils/mocks'
 
 const electronicsAxios = defineAxios(mocks.electronics)
 
+function formatProductsByPageId(
+  products: Product[],
+  pageQuery: string,
+  perPageQuery: string,
+) {
+  const pageId = +pageQuery
+  const perPageAmount = +perPageQuery
+
+  const startIndex = perPageAmount * pageId
+  const endIndex = perPageAmount * pageId + perPageAmount
+
+  return products.slice(startIndex, endIndex)
+}
+
 const getElectronicsAll = async (): Promise<Product[] | Error> =>
   electronicsAxios
     .get('/electronics/all')
@@ -14,7 +28,26 @@ const getElectronicsAll = async (): Promise<Product[] | Error> =>
       return new Error(error)
     })
 
-const getElectronicProductById = async (id: number): Promise<Product | Error> =>
+const getElectronicsByQuery = async (
+  pageQuery: string,
+  perPageQuery: string,
+): Promise<Product[] | Error> =>
+  electronicsAxios
+    .get<Product[]>(
+      '/electronics/all',
+      {},
+      {
+        formatter: (products) =>
+          formatProductsByPageId(products, pageQuery, perPageQuery),
+      },
+    )
+    .then(({ data }) => data)
+    .catch(({ response }) => {
+      const error = response?.data?.error ?? 'Ошибка получения товаров.'
+      return new Error(error)
+    })
+
+const getElectronicProductById = async (id: string): Promise<Product | Error> =>
   electronicsAxios
     .get(`/electronics/${id}`, {}, { params: { id } })
     .then(({ data }) => data)
@@ -25,5 +58,6 @@ const getElectronicProductById = async (id: number): Promise<Product | Error> =>
 
 export const ElectronicsService = {
   getElectronicsAll,
+  getElectronicsByQuery,
   getElectronicProductById,
 }
